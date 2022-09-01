edd19ab5138885bfed059a7aa5d4d03d3b4c8fcf
avoreshin <avoreshin@gmail.com>
2022-09-01 23:25:50 +0300

    feat: add Navbar components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => (
       direction="row"
       alignItems="center"
       p={2}
-      sx={{ positoin: 'stycky', background: '#000', top: 0, justifyContent: 'space-between' }}
+      sx={{ position: 'sticky', background: '#000', top: 0, justifyContent: 'space-between' }}
     >
       <Link to="/"
         style={{ display: "flex", alignItems: "center" }}>
