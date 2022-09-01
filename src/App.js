d1c2330b07195465d0036625740db21cecd4134d
avoreshin <avoreshin@gmail.com>
2022-09-01 23:24:49 +0300

    feat: add SearchBar

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import { Box } from "@mui/material";
+import {Box} from "@mui/material";
 import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Feed from './components/Feed'
-import Navbar from './components/Navbar'
-import VideoDetail from './components/VideoDetail'
-import ChanelDetail from './components/ChanelDetail'
-import SearchFeed from './components/SearchFeed'
+import {Feed, Navbar, SearchFeed, VideoDetail, ChanelDetail} from './components'
 
 
 function App() {
-  return (
-    <BrowserRouter>
-    <Box sx={{background: '#000'}}>
-<Navbar/>
-<Routes>
-  <Route path='/' exact element={<Feed/>}/>
-  <Route path="/video/:id" element={<VideoDetail/>}/>
-  <Route path="/chanel/:id" element={<ChanelDetail/>}/>
-  <Route path="/search/:searchTerm" element={<SearchFeed/>}/>
-</Routes>
-    </Box>
-    </BrowserRouter>
-  );
- }
+    return (
+        <BrowserRouter>
+            <Box sx={{background: '#000'}}>
+                <Navbar/>
+                <Routes>
+                    <Route path='/' exact element={<Feed/>}/>
+                    <Route path="/video/:id" element={<VideoDetail/>}/>
+                    <Route path="/chanel/:id" element={<ChanelDetail/>}/>
+                    <Route path="/search/:searchTerm" element={<SearchFeed/>}/>
+                </Routes>
+            </Box>
+        </BrowserRouter>
+    );
+}
 
 export default App;
