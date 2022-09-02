2589f7fdfce1ef38eadcc26f715f9b3575af8107
avoreshin <avoreshin@gmail.com>
2022-09-02 22:50:35 +0300

    feat: update index components

--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,8 @@ export {default as ChanelDetail} from './ChanelDetail'
 export {default as SearchFeed} from './SearchFeed'
 export {default as VideoDetail} from './VideoDetail'
 export {default as SearchBar} from './SearchBar'
+export {default as SideBar} from './SideBar'
+export {default as Videos} from './Videos'
+export {default as VideoCard} from './VideoCard'
+export {default as ChannelCard} from './ChannelCard'
 
