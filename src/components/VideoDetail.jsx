d88800277b77208a58bfc35a33280a23c852916a
avoreshin <avoreshin@gmail.com>
2022-09-05 00:26:16 +0300

    feat: fix

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -11,17 +11,19 @@ const VideoDetail = () => {
 
     const {id} = useParams();
     const [videoDetail, setVideoDetail] = useState(null);
+    const [videos, setVideos] = useState(null);
+
     useEffect(() => {
         fetchFromAPI(`videos?part=snippet,statistics&id=${id}`)
             .then((data) => setVideoDetail(data.items[0]))
+        fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`)
+            .then((data => setVideos(data.items)))
     }, [id])
 
     if (!videoDetail?.snippet) return 'Loading...'
 
     const {
-        snippet: {title, channelId, channelTitle}, statistics: {
-            viewCount, likeCount
-        }
+        snippet: {title, channelId, channelTitle}, statistics: {viewCount, likeCount}
     } = videoDetail;
 
     return (
@@ -37,31 +39,51 @@ const VideoDetail = () => {
                                      className="react-player"
                                      controls
                         />
-                        <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+                        <Typography color="#fff"
+                                    variant="h5"
+                                    fontWeight="bold"
+                                    p={2}>
                             {title}
                         </Typography>
-                        <Stack
-                            direction="row"
-                            justifyContent="space-between"
-                            py={1}
-                            px={2}
-                            sx={{color: '#fff'}}
+                        <Stack direction="row"
+                               justifyContent="space-between"
+                               py={1}
+                               px={2}
+                               sx={{color: '#fff'}}
                         >
                             <Link to={`/channel/${channelId}`}>
-                                <Typography variant={{xm: 'subtitle1', md: "h6"}}
-                                            color="#fff"
+                                <Typography
+                                    variant={{xm: 'subtitle1', md: "h6"}}
+                                    color="#fff"
                                 >
                                     {channelTitle}
-                                    <CheckCircle sx={{fontSize: '12px', color: "gray", ml: "5px"}}/>
+                                    <CheckCircle sx={{
+                                        fontSize: '12px',
+                                        color: "gray",
+                                        ml: "5px"
+                                    }}/>
                                 </Typography>
                             </Link>
-                            <Stack>
-                                <Typography variant="body1" sx={{opacity:0.7}}>
-                                    {viewCount} views
+                            <Stack direction="row"
+                                   gap="20px"
+                                   alignItems="center">
+                                <Typography variant="body1" sx={{opacity: 0.7}}>
+                                    {parseInt(viewCount).toLocaleString()} views
+                                </Typography>
+                                <Typography variant="body1" sx={{opacity: 0.7}}>
+                                    {parseInt(likeCount).toLocaleString()} likes
                                 </Typography>
                             </Stack>
                         </Stack>
                     </Box>
+                </Box>
+                <Box
+                    px={2}
+                    py={{md: 1, xs: 5}}
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Videos videos={videos} direction="column"/>
                 </Box>
             </Stack>
         </Box>
